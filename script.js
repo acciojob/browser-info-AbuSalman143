b9f9a2a8acb1260494e3de6d6a7d5b301b43a5de
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-30 12:10:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let info=document.getElementById("browser-info");
+let browserName=window.navigator.appName;
+let  version =window.navigator.appVersion;
+
+info.innerText=`You are using ${browserName} version ${version}`;
